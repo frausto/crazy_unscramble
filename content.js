4a49bdd61607ec168db175e291acabca10914f5b
Nolan Frausto <nrfrausto@gmail.com>
2013-01-26 14:10:17 -0500

    styling, and make work on ajax loads

--- content.js
+++ content.js
@@ -1,17 +1,47 @@
-console.log("START");
-
-imageUrl = $(".instadate .thumbnail img").attr('src');
 var puzzleHtml = chrome.extension.getURL('puzzle.html');
 
-$('.instadate').load(puzzleHtml, function(){
-  tiles = $(this).find('img');
-  tiles.attr('src', imageUrl);
-  tiles.addClass(function(){
-    parentPosition = $(this).parent().position();
-    $(this).css('top', parentPosition.top * -1  + 'px')
-    $(this).css('left', parentPosition.left * -1  + 'px')
+makeIntoPuzzles = function(){
+  if ($(".instadate .thumbnail img").length < 1){
+    return;
+  }
+
+  $('#instadates').prepend("<div class='unscramble-header'>You are using the Crazy Unscrambler Chrome Extension!</div><div class='unscramble-small'>Click a picture tile, and then another to swap them. Now you can start unscrambling some crazies!</div>");
+  clearInterval(refreshIntervalId);
+
+  $(".instadate .thumbnail img").each(function(i,pic){
+    var imageUrl = $(pic).attr('src');
+    $(pic).parent().load(puzzleHtml, function(){
+      tiles = $(this).find('img');
+      tiles.attr('src', imageUrl);
+      tiles.addClass(function(){
+        parentPosition = $(this).parent().position();
+        $(this).css('top', parentPosition.top * -1  + 'px');
+        $(this).css('left', parentPosition.left * -1  + 'px');
+      });
+
+      var clickedTile = null;
+
+      $('.unscramble-tile').click(function(){
+        if ($(this).hasClass('unscramble-clicked')){
+          $('.unscramble-clicked').removeClass('unscramble-clicked');
+          return;
+        }
+        $('.unscramble-clicked').removeClass('unscramble-clicked');
+
+        if(clickedTile == null){
+          clickedTile = $(this).find('img');
+          $(this).addClass('unscramble-clicked');
+        }else{
+          second = $(this).find('img');
+          firstStyle = clickedTile.attr('style');
+          secondStyle = second.attr('style');
+          clickedTile.attr('style', secondStyle);
+          second.attr('style', firstStyle);
+          clickedTile = null;
+        }
+      });
+    });
   });
-});
+};
 
-
-console.log("END");
+var refreshIntervalId = setInterval(makeIntoPuzzles, 1000);
